7c6377061f57131f75c8477c4d5019e81979e5d6
Mitros27 <trosmi04@gmail.com>
2026-03-04 00:32:49 +0100

    Add angular material

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import axios from 'axios';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
